84449ae72181e5dd9d4a6d4848a8681551e7edd5
Edin Kadribasic <edink@php.net>
2007-05-26 16:30:56 +0000

    News snaps index by John Mertic

--- snaps_index.php
+++ snaps_index.php
@@ -1,271 +1,337 @@
 <?php
-/* Developed by Ilia Alshanetsky (ilia@prohost.org) 
-   Modified by Kornel Lesinski (kornel@ideadesigners.co.uk)
+/*
+   +----------------------------------------------------------------------+
+   | PHP Infrastucture                                                    |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2007 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: John Mertic <jmertic@php.net>                               |
+   |          Edin Kadribasic <edink@php.net>                             |
+   +----------------------------------------------------------------------+
 */
 
-/* Rules */
-$__RULES = array(
-	'source'=> array('.', 2, 2, array('.gz'=>1, '.bz2'=>1), 'header'=>'Source Distribution', 'info'=>array(
-			'STABLE4'=>array('header'=>'Stable (4.4.x-dev)'),
-			'STABLE5'=>array('header'=>'Stable (5.2.x-dev)'),
-			'UNSTABLE'=>array('header'=>'Latest CVS (6.0-dev)'),
-		)),
-	'win32'=> array('win32', 8, 4, array('.zip'=>1), 'header'=>'Win32 Packages',	'info'=>array(
-				'STABLE4'=>array(
-					'header'=>'Stable (4.4.x-dev)', 
-					'pecl'=>array('http://pecl4win.php.net/list.php/4_4','PECL extensions for the 4.4.x win32 branch'),
-					'compilelog'=>'win32/compile-STABLE.log',
-					'snaplog'=>'win32/snapshot-STABLE.log',
-					),
-				'STABLE5'=>array(
-					'header'=>'Stable (5.2.x-dev)', 
-					'pecl'=>array('http://pecl4win.php.net/list.php/5_2','PECL extensions for the 5.2.x branch'),
-					'snaplog'=>'win32/snapshot-5.2.log',
-					'debugpack'=>'/win32/php5.2-dbgpack-win32-latest.zip',
-					),
-				'UNSTABLE'=>array(
-					'header'=>'Latest CVS (6.0-dev)', 
-					'pecl'=>array('http://pecl4win.php.net/list.php/6_0','PECL extensions for the 6.0 win32 branch'),
-					'snaplog'=>'win32/snapshot-6.0.log',
-					'debugpack'=>'/win32/php6.0-dbgpack-win32-latest.zip',
-					),
-			)),
-);
+/* $Id$ */
 
-$__CTIME = time();
-$__LB_TIME = array();
-
-
-header("Content-Type:text/html;charset=ISO-8859-1");
-?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-	<title>PHP Snapshots</title>
-	<style type="text/css">
-		html {font: 0.75em verdana, sans-serif;}
-		body {margin:0;padding:0;font-size:100.01%}
-		div.distr  {overflow:auto; clear:left; padding: 5px 10px 10px}
-		div.distr div {width:33%;min-width:12em; float:left;}
-		#index {display:none}
-		h1 {margin:0;font-size:190%;padding: 18px 0 0 133px; min-height: 49px; background: #99c 3px 50% url(http://static.php.net/www.php.net/images/php.gif) no-repeat; border-bottom: 1px solid #336;}
-		* html h1 {height: 49px;}
-		#datetime {margin:0;padding:5px;color:white;text-align:right;background:#669; font-size: 9px; border-bottom: 1px solid #336;}
-		
-		h2,h3 {color:#006; font-size: 135%; margin:0.3em 0 0.4em;}
-		h3,#nextsnap h2 {font-size: 120%;}
-	
-		
-		ul,li {margin:0;padding:0;list-style:none;}
-		li {margin-bottom:1em;}
-		li.first {background: #f3f5ff; padding: 3px 0;}
-
-		.date,.filelink {white-space:nowrap;}
-		.filelink {margin-right:0.5em;}
-		
-		#snaps {padding: 0 0 3px 200px; position:relative;}
-		* html #snaps {height:1%;}
-		#nextsnap {
-			position:absolute;
-			width:190px; padding:0 5px;
-			height:100%;
-			height: expression(this.offsetParent.offsetHeight);
-			left:0;top:0; border-right: 1px dashed #777; background:#f0f0f0; margin-right:-1px;
-		}
-		#nextsnap dt {margin-top:1em;}
-		#nextsnap dd {margin:0;padding:0;}
-		#nextsnap em {color: red;font-style:normal;}
-		th {text-align:left;}
-		
-		a img {border:0;float:left;margin-right: 3px}
-		p {clear:left;}
-		
-		#footer {padding: 2px; font-size: 0.9em; background:#ccc; border-top:#336 1px solid; text-align:right;}
-	
-		</style>
-</head>
-<body>
-<h1>PHP Snapshots</h1>
-<p id="datetime"><?php echo pdate(time()); ?></p>
-<?php
-
-function next_snap_prediction($current_time, $last_time, $freq, $sn_log='', $stable='')
-{
-	$to = ($last_time + ($freq * 3600)) - $current_time;
-	
-	if ($to < 0) {
-		$stable = $stable ? '-STABLE' : '';
-		if ($to > -600) {
-			$res = '<em>any moment now.</em>';
-		} else {
-			$res = '<em>please consult /dev/urandom</em>';
-		}
-	} else {
-		$h = floor($to/3600);
-		$m = floor(($to - $h * 3600) / 60);
-		
-		if ($h) {
-			$res = $h . ' hour'.($h==1?'':'s').' and ' . $m . ' minute'.($m==1?'':'s');
-		} else {
-			$res = $m . ' minute'.($m==1?'':'s');		
-		}
-	}
-	
-	return $res;
+/* Show source */
+if ($_SERVER['QUERY_STRING'] === "source") {
+  highlight_file($_SERVER['SCRIPT_FILENAME']);
+  die();
 }
 
-function get_ext($file_name)
+/* All times on this site are UTC */
+date_default_timezone_set("UTC");
+
+/* Configure branches and distributions */
+$b = array();
+
+/* 4.4 */
+$b["4.4"] = 
+  array(
+  "Source" => array(
+    "freq" => 3600 * 2,
+    "glob" => array(
+      "php-4.4-dev (tar.bz2)" => "php4-STABLE-2*.tar.bz2",
+      "php-4.4-dev (tar.gz)"  => "php4-STABLE-2*.tar.gz"
+      ),
+  ),
+  "Win32" => array(
+    "freq" => 3600 * 8,
+    "glob" => array(
+      "php-4.4-dev (zip)" => "win32/php4-win32-STABLE-2*.zip",
+      "Snapshot log"  => "win32/snapshot-STABLE.log"
+    ),
+  ),
+  );
+
+/* 5.2 */
+$b["5.2"] = 
+  array(
+  "Source" => array(
+    "freq" => 3600 * 2,
+    "glob" => array(
+      "php-5.2-dev (tar.bz2)" => "php5.2-2*.tar.bz2",
+      "php-5.2-dev (tar.gz)"  => "php5.2-2*.tar.gz"
+      ),
+  ),
+  "Win32" => array(
+    "freq" => 3600 * 4,
+    "glob" => array(
+      "php-5.2-dev (zip)" => "win32/php5.2-win32-2*.zip",
+      "php-5.2-dev (msi)" => "win32/php5.2-win32-installer-2*.msi",
+      "pecl-5.2-dev (zip)" => "win32/pecl5.2-win32-2*.zip",
+      "Debug pack"  => "win32/php5.2-dbgpack-win32-2*.zip",
+      "Snapshot log"  => "win32/snapshot-5.2.log",
+    ),
+  ),
+  );
+
+/* 6.0 */
+$b["6.0"] = 
+  array(
+  "Source" => array(
+    "freq" => 3600 * 2,
+    "glob" => array(
+      "php-6.0-dev (tar.bz2)" => "php6.0-2*.tar.bz2",
+      "php-6.0-dev (tar.gz)"  => "php6.0-2*.tar.gz"
+      ),
+  ),
+  "Win32" => array(
+    "freq" => 3600 * 8,
+    "glob" => array(
+      "php-6.0-dev (zip)" => "win32/php6.0-win32-2*.zip",
+      "Snapshot log"  => "win32/snapshot-6.0.log",
+    ),
+  ),
+  );
+
+function get_file_info($glob, $freq = 0)
 {
-	return strrchr($file_name, '.');
-}
+  static $cache = array();
 
-function get_name($file_name)
-{
-	return preg_replace('!^.*?/(php[\d.]+).*?\.(.+)$!','\1 (\2)',$file_name);
-}
+  if (isset($cache[$glob])) {
 
+    return $cache[$glob];
 
-function stz_to_ts($value)
-{
-	$year = substr($value, 0, 4);
-	$month = substr($value, 4, 2);
-	$day = substr($value, 6, 2);
-	$hour = substr($value, 8, 2);
-	$min = substr($value, 10, 2);
+  } else {
 
-	return gmmktime($hour, $min, 0, $month, $day, $year);	
-}
+    $ret = new stdClass;
+    $g = @glob($glob);
 
-function print_fsize($file)
-{
-	return sprintf("%.1f", filesize($file)/1024/1024);
-}
+    if (!is_array($g)) {
+      return $ret;
+    }
 
-function get_ts($file_name)
-{
-	$s = strrpos($file_name, '-') + 1;
-	$e = strpos($file_name, '.', $s);
-	
-	$tm_s = substr($file_name, $s, ($e-$s));
-	
-	return stz_to_ts($tm_s);
-}
+    $g = array_reverse($g);
 
-function pdate($time)
-{
-	return gmdate("M d, Y H:i T", $time);
-}
+    $ret->files = array();
+    $now = time();
+    $first = true;
 
-function draw_directory($job)
-{
-	$files = array('STABLE4'=>array(),'STABLE5'=>array(),'UNSTABLE'=>array());
+    foreach($g as $filename) {
 
-	if ($dir = opendir($job[0]))
-	{
-		while ($file = readdir($dir)) {
+      $f = new stdClass;
+
+      $f->name = $filename;
+      $f->size = @filesize($f->name);
+      $f->size_str = sprintf("%.1f",$f->size/1024/1024);
+ 
+      if (preg_match("((\\d{12}))", $f->name, $m)) {
+
+	$t = @strtotime($m[1]);
+	$f->time = $t;
+	$f->time_str = str_replace(" ", "&nbsp;", date("M d, Y H:i T", $f->time));
+
+	if ($freq) {
+	  
+	  $to = $t + $freq - $now;
+
+	  if ($to < 0) {
+	    if ($to > -600) {
+	      $res = '<em>any moment now.</em>';
+	    } else {
+	      $res = '<em>please consult /dev/urandom</em>';
+	    }
+	  } else {
+	    $h = floor($to/3600);
+	    $m = floor(($to - $h * 3600) / 60);
 		
-			if ($file=='.' || $file=='..') continue;
-		
-			$file = $job[0] . '/' . $file;
-		
-			if (is_link($file) || !is_file($file) || !isset($job[3][get_ext($file)]) || strpos($file, 'dbgpack')) {
-				continue;
-			}
-			if (strpos($file, 'php4-STABLE') || strpos($file, 'php4-win32-STABLE')) {
-				$files['STABLE4'][get_ts($file)][] = $file;
-			} else if (strpos($file, 'php5.2')) {
-				$files['STABLE5'][get_ts($file)][] = $file;
-			} else if (strpos($file, 'php6')) {
-				$files['UNSTABLE'][get_ts($file)][] = $file;
-			}	
-		}
-	
-		closedir($dir);
+	    if ($h) {
+	      $res = $h . ' hour'.($h==1?'':'s').' and ' . $m . ' minute'.($m==1?'':'s');
+	    } else {
+	      $res = $m . ' minute'.($m==1?'':'s');		
+	    }
+	  }
 
-		krsort($files['STABLE4']);
-		krsort($files['STABLE5']);
-		krsort($files['UNSTABLE']);
+	  $f->next = $res;
 	}
 
-	return $files;
+      }
+
+      $ret->files[] = $f;
+    }
+
+    return $ret;
+  }
 }
 
-echo '<ul id="index">';
-foreach($__RULES as $rulename => $rules)
+function display_next_snap($conf)
 {
-	echo '<li>'.$rules['header'].'<ul>';
-	foreach(draw_directory($rules) as $section => $result)
-	{
-		echo '<li><a href="#'.strtolower($rulename.'-'.$section).'">'.$rules ['info'][$section]['header'].'</a></li>';
-	}
-	echo '</ul></li>';
-}
-echo '<li><a href="#nextsnap">Next snapshot prediction</a></ul><div id="snaps">';
-
-foreach($__RULES as $rulename => $rules)
-{
-	echo '<div class="distr"><h2>'.$rules['header'].'</h2>';
-	foreach(draw_directory($rules) as $section => $result)
-	{
-		echo '<div id="'.strtolower($rulename.'-'.$section).'"><h3>'.$rules ['info'][$section]['header'].'</h3>';
-		echo '<ul>';
-		foreach ($result as $tm => $files) 
-		{
-			if (!isset($__LB_TIME[$rulename.$section]))
-			{
-				$__LB_TIME[$rulename.$section] = $tm;
-				echo '<li class="first">';
-			}
-			else echo '<li>';
-			
-			sort($files);
-			foreach($files as $file) 
-			{
-				echo '<span class="filelink"><a href="'.$file.'">'.get_name($file).'</a> ('.print_fsize($file).'M)</span> ';
-			}
-			echo '<br><b>Built On:</b> <span class="date">'.pdate($tm).'</span></li>';
-		}
-		echo '</ul>';
-		
-		if (isset($rules ['info'][$section]['compilelog']))
-		{
-			echo '<p><a href="'.$rules ['info'][$section]['compilelog'].'">Compile log</a></p>';
-		}
-		if (isset($rules ['info'][$section]['snaplog']))
-		{
-			echo '<p><a href="'.$rules ['info'][$section]['snaplog'].'">Snapshot log</a></p>';
-		}		
-		if (isset($rules ['info'][$section]['debugpack']))
-		{
-			echo '<p><a href="'.$rules ['info'][$section]['debugpack'].'">Debug pack</a></p>';
-		}	
-		if (isset($rules ['info'][$section]['pecl']))
-		{
-			echo '<p><a href="'.$rules ['info'][$section]['pecl'][0].'"><img src="http://snaps.php.net/images/pecl-icon.png" alt="">'.$rules ['info'][$section]['pecl'][1].'</a></p>';
-		}
-
-		echo '</div>';
-	}
-	echo '</div>';
+  foreach($conf as $branch_name => $branch) {
+    foreach($branch as $distro_name => $distro) {
+      foreach($distro["glob"] as $glob) break;
+      $info = get_file_info($glob, $distro["freq"]);
+      echo "<dt><b>PHP {$branch_name} {$distro_name}</b> in</dt><dd>{$info->files[0]->next}</dd>";
+    }
+  }
 }
 
-?>			
 
-<div id="nextsnap">
-	<h2>Next snapshot</h2>
-	<dl><dt><b>STABLE source</b> in</dt><dd><?php 
-echo next_snap_prediction($__CTIME, $__LB_TIME['sourceSTABLE4'], $__RULES['source'][1]); ?>
-		</dd>	<dt><b>Latest CVS source</b> in</dt><dd><?php 
-echo next_snap_prediction($__CTIME, $__LB_TIME['sourceUNSTABLE'], $__RULES['source'][2]); ?>
-		</dd>			<dt><b>STABLE 4.4.x Win32</b> in</dt><dd><?php 
-echo next_snap_prediction($__CTIME, $__LB_TIME['win32STABLE4'], 8, 'win32', 1); ?>
-		</dd>			<dt><b>STABLE 5.2.x Win32</b> in</dt><dd><?php 
-echo next_snap_prediction($__CTIME, $__LB_TIME['win32STABLE5'], 4, 'win32', 1); ?>
-		</dd>		<dt><b>Latest CVS Win32</b> in</dt><dd><?php 
-echo next_snap_prediction($__CTIME, $__LB_TIME['win32UNSTABLE'], 8, 'win32'); ?>		
-		</dd></dl></div>
-	</div>
-	<div id="footer">
-		<a href="http://cvs.php.net/viewcvs.cgi/systems/snaps_index.php?view=markup">source</a> | <a href="http://php.net">php.net</a> | <a href="http://www.php.net/anoncvs.php">anonymous CVS</a> | 		<a href="http://www.php.net/downloads.php">stable releases</a>
-	</div>
-</body>
-</html>
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>PHP Snapshots</title>
+        <style type="text/css" media="all">
+        @import url("http://static.php.net/www.php.net/styles/site.css");
+        @import url("http://static.php.net/www.php.net/styles/phpnet.css");
+        </style>
+        <!--[if IE]><![if gte IE 6]><![endif]-->
+        <style type="text/css" media="print">
+        @import url("http://static.php.net/www.php.net/styles/print.css");
+        </style>
+        <!--[if IE]><![endif]><![endif]-->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="shortcut icon" href="http://static.php.net/www.php.net/favicon.ico" />
+    </head> 
+    <body>
+        <div id="headnav">
+            <a href="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>"><img src="http://static.php.net/www.php.net/images/php.gif"
+            alt="PHP" width="120" height="67" /></a>
+            <div id="headmenu">
+                <a href="http://www.php.net/downloads.php">downloads</a> |
+                <a href="http://www.php.net/docs.php">documentation</a> |
+                <a href="http://www.php.net/FAQ.php">faq</a> |
+                <a href="http://www.php.net/support.php">getting help</a> |
+                <a href="http://www.php.net/mailing-lists.php">mailing lists</a> |
+                <a href="http://bugs.php.net/">reporting bugs</a>
+            </div>
+        </div>
+        <div id="headsearch" style="margin-bottom: 0;">
+            <p>
+                <span><?php echo date("M d, Y H:i T") ?></span>
+            </p>
+        </div>
+        <div id="layout_2">
+            <div id="leftbar">
+                <h3>Snapshot Builds</h3>
+                <p>
+                    You can find the latest development source and binary 
+                    packages for PHP 4, PHP 5, and PHP 6 here. Source code is 
+                    checked out of CVS and packaged every 2 hours and Win32 
+                    builds are produced every 8 hours.
+                </p>
+                <p>
+                    These packages are <strong>NOT</strong> intended for 
+                    production use; please use the packages at 
+                    <a href="http://www.php.net/downloads">PHP downloads</a>.
+                </p>
+                
+                <h3>Next snapshot</h3>
+                <dl><?php display_next_snap($b); ?></dl>
+
+            </div>
+            
+            <div id="content" class=".">
+                <div id="snaps">
+                    <h1>PHP Snapshots</h1>
+
+<?php   
+if (isset($_GET['branch']) && isset($_GET['distro']) && isset($b[$_GET['branch']][$_GET['distro']])) {
+
+  $distro = $b[$_GET['branch']][$_GET['distro']];
+
+  echo "<h2>PHP {$_GET['branch']} {$_GET['distro']}</h2>\n";
+
+  echo '<table border="0" cellpadding="3" cellspacing="2" class="standard"><tr>';
+
+  foreach ($distro["glob"] as $title => $glob) {
+    echo "<th>$title</th>\n";
+  }
+
+  echo "</tr><tr>";
+
+  foreach ($distro["glob"] as $title => $glob) {
+    $info = get_file_info($glob);
+    echo "<td valign=\"top\">";
+    for ($i=0; $i<count($info->files); $i++) {
+      echo "<span><a href=\"{$info->files[$i]->name}\">{$title}</a></span><br />\n";
+      echo "<span style=\"font-size: .8em\">Size: {$info->files[$i]->size_str}M</span><br />\n";
+      echo "<span style=\"font-size: .8em\">{$info->files[$i]->time_str}</span><br />\n";
+      echo "<br />";
+    }
+    echo "</td>";
+  }
+
+  echo "</tr></table>";
+
+} else {
+
+  foreach($b as $branch_name => $branch) {
+
+    echo "<h2>PHP $branch_name</h2>\n";
+    echo '<table border="0" cellpadding="3" cellspacing="2" class="standard">';
+    echo "<tr>\n";
+
+    foreach($branch as $distro_name => $distro) {
+      echo "<th>$distro_name</th>";
+    }
+
+    echo "</tr>\n<tr>\n";
+
+    foreach($branch as $distro_name => $distro) {
+
+      echo "<td valign=\"top\">\n";
+      
+      foreach ($distro["glob"] as $title => $glob) {
+	$info = get_file_info($glob);
+	$f = $info->files[0];
+
+	echo "<span><a href=\"{$f->name}\">{$title}</a> ({$f->size_str}M)</span><br />\n";
+      }
+      
+      echo "<br /></td>\n";
+      
+    }
+
+    echo "</tr>\n<tr>\n";
+
+    foreach($branch as $distro_name => $distro) {
+
+      echo "<td>\n";
+      foreach ($distro["glob"] as $glob) break;
+      $info = get_file_info($glob);
+      $f = $info->files[0];
+      echo "<b>Built on:</b> " . $f->time_str ."<br />\n";
+      echo "<span class=\"filelink\"><a href=\"" . htmlentities($_SERVER['PHP_SELF']). "?branch=" . urlencode($branch_name) . "&distro=" . urlencode($distro_name) . "\">Previous Builds</a></span><br />\n";
+
+      echo "</td>\n";
+      
+    }
+
+    echo "</tr>\n</table>\n";
+
+  }
+}   
+
+?>
+                </div>
+            </div>
+            <div class="cleaner">&nbsp;</div>
+        </div>
+        
+        <div id="footnav">
+            <a href="<?php echo htmlentities($_SERVER['PHP_SELF']) . "?source" ?>">show source</a>   
+        </div>
+        
+        <div id="pagefooter">
+            <div id="copyright">
+                <a href="http://www.php.net/copyright.php">Copyright &copy; 2001-2007 The PHP Group</a><br />
+                All rights reserved.
+            </div>
+        
+            <div id="thismirror">
+                <br />
+                Last updated: <?php echo  date("M d, Y H:i T", filemtime($_SERVER['SCRIPT_FILENAME'])) ?>
+            </div>
+        </div> 
+    </body> 
+</html> 
