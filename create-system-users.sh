e93072640f9baacc71b787f51290f74d209c6bee
Derick Rethans <github@derickrethans.nl>
2024-10-18 09:48:51 +0100

    Use realpath to improve my loop
    Co-authored-by: Jim Winstead <jimw@trainedmonkey.com>

--- create-system-users.sh
+++ create-system-users.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Figuring out where we are
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 # Find users file
 HOSTNAME=$( hostname )
